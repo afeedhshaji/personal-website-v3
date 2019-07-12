33edf9fe9fa74bd75a0187a35e0872103ce9205b
afeedh <afeedh_b170282cs@nitc.ac.in>
2019-07-12 23:33:54 +0530

    Moved the scripts to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
 
-
-
     /* Baffle JS */
     const text = baffle(".main-heading");
     text.set({
@@ -13,6 +11,15 @@ $(document).ready(function () {
     // Reveal your text over 1000ms
     text.reveal(70000);
 
+    /* Typed JS */
+    var typed = new Typed('#typed', {
+        strings: ["Developer.^1000 ", "ML/DL Enthusiast.^1000 ", "Programmer.^1000 "],
+        stringsElement: null,
+        loop: true,
+        typeSpeed: 80,
+        backSpeed: 80,
+
+    });
 
 });
 
