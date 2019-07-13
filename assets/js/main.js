9870c65935559f9b696be7a446e12bbcd0abe0ac
afeedh <afeedh_b170282cs@nitc.ac.in>
2019-07-14 01:36:03 +0530

    Added iron man css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,36 @@
 $(document).ready(function () {
 
+    //Preloader
+    $(window).on("load", function () {
+        preloaderFadeOutTime = 200;
+        setTimeout(() => {
+            var preloader = $('.preloader');
+            preloader.fadeOut(preloaderFadeOutTime);
+        }, 4000);
+    });
+
     /* Baffle JS */
-    const text = baffle(".main-heading");
-    text.set({
-        characters: 'Afeedh Shaji',
-        speed: 95
-    });
-    text.start();
-    text.reveal(70000);
 
-    /* Typed JS */
-    var typed = new Typed('#typed', {
-        strings: ["Developer.^1000 ", "ML/DL Enthusiast.^1000 ", "Programmer.^1000 ", "Designer.^1000 "],
-        stringsElement: null,
-        loop: true,
-        typeSpeed: 80,
-        backSpeed: 80,
-    });
+    setTimeout(() => {
+        const text = baffle(".main-heading");
+        text.set({
+            characters: 'Afeedh Shaji',
+            speed: 95
+        });
+        text.start();
+        text.reveal(70000);
+    }, 3200);
 
+    setTimeout(() => {
+        /* Typed JS */
+        var typed = new Typed('#typed', {
+            strings: ["Developer.^1000 ", "ML/DL Enthusiast.^1000 ", "Programmer.^1000 ", "Designer.^1000 "],
+            stringsElement: null,
+            loop: true,
+            typeSpeed: 80,
+            backSpeed: 80,
+        });
+    }, 3200)
 
 
 });
