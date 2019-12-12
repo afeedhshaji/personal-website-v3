c9ec11a8783b4984e21a5c044c3a54e27b127241
Afeedh Shaji <afeedh_b170282cs@nitc.ac.in>
2019-12-12 22:41:52 +0530

    UI update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,40 @@
-$(document).ready(function () {
-
-
-
-    /* Baffle JS */
-
+$(document).ready(function() {
+  //Fade out preloader
+  $(window).on("load", function() {
+    var preloaderFadeOutTime = 300;
+    function hidePreloader() {
+      var preloader = $(".preloader");
+      preloader.fadeOut(preloaderFadeOutTime);
+    }
     setTimeout(() => {
-        const text = baffle(".main-heading");
-        text.set({
-            characters: 'Afeedh Shaji',
-            speed: 95
-        });
-        text.start();
-        text.reveal(70000);
-    }, 2500);
+      hidePreloader();
+    }, 1500);
+  });
+  /* Baffle JS */
 
-    setTimeout(() => {
-        /* Typed JS */
-        var typed = new Typed('#typed', {
-            strings: ["Developer.^1000 ", "ML/DL Enthusiast.^1000 ", "Programmer.^1000 ", "Designer.^1000 "],
-            stringsElement: null,
-            loop: true,
-            typeSpeed: 80,
-            backSpeed: 80,
-        });
-    }, 3200)
+  setTimeout(() => {
+    const text = baffle(".main-heading");
+    text.set({
+      characters: "Afeedh Shaji",
+      speed: 85
+    });
+    text.start();
+    text.reveal(70000);
+  }, 100);
 
-
+  setTimeout(() => {
+    /* Typed JS */
+    var typed = new Typed("#typed", {
+      strings: [
+        "Developer.^1000 ",
+        "ML/DL Enthusiast.^1000 ",
+        "Programmer.^1000 ",
+        "Designer.^1000 "
+      ],
+      stringsElement: null,
+      loop: true,
+      typeSpeed: 80,
+      backSpeed: 80
+    });
+  }, 600);
 });
-
